9e6929756fa4012d67f9bd26d3cc88368929d12a
Xinnji <xinnji@gmail.com>
2021-11-17 12:53:24 -0800

    Adding config.ini to app.py and git ignore. Added support for SQL queries to app.py.

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template
-# import MySQLdb
+import MySQLdb
 import os
+import configparser
 
 
 app = Flask(__name__)
 
-# people = ["id", "name", "age", "favMovie", "favShow", "favBook", "favGame", "seenMovies", "seenShows", "readBooks"]
+# people = ["id", "name", "age", "favMovie", "favShow", "favBook", "favGame"]
 # movies = ["id", "title", "genre", "director", "runTimeMins", "metacritic"]
 # shows = ["id", "title", "genre", "network", "episodes", "seasons", "metacritic"]
 # books = ["id", "title", "genre", "author", "pages", "metacritic"]
 # videogames = ["id", "title", "genre", "studio", "playTimHrs", "metacritic"]
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+con = MySQLdb.connect(host=config['SQL']['host'],
+                      user=config['SQL']['user'],
+                      passwd=config['SQL']['passwd'],
+                      db=config['SQL']['db'])
+cursor = con.cursor(MySQLdb.cursors.DictCursor)
+# For each query
+# cursor.execute('query')
+# con.commit();
 
 
 @app.route('/', methods=["GET", "POST"])
