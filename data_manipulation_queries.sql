9da1bfeaf3c93daea40b222d0f4b011459bb2c0f
Nicholas Broce <55900341+broceni@users.noreply.github.com>
2021-11-18 12:47:26 -0800

    Updated SQL Commands

--- data_manipulation_queries.sql
+++ data_manipulation_queries.sql
@@ -6,40 +6,70 @@ JOIN Movies ON Movies.id = favMovie
 JOIN Shows ON Shows.id = favShow
 JOIN Books ON Books.id = favBook
 JOIN VideoGames ON VideoGames.id = favGame;
+-- Get all the info about what people have watched/read/played
+SELECT People.id, title FROM Movies
+JOIN seenMovies ON Movies.id = seenMovies.movies_id
+JOIN People on seenMovies.people_id = People.id
+SELECT People.id, title FROM Books
+JOIN readBooks ON Books.id = readBooks.books_id
+JOIN People on readBooks.people_id = People.id
+SELECT People.id, title FROM Shows
+JOIN seenShows ON Shows.id = seenShows.shows_id
+JOIN People on seenShows.people_id = People.id
+SELECT People.id, title FROM VideoGames
+JOIN playedGames ON VideoGames.id = playedGames.video_games_id
+JOIN People on playedGames.people_id = People.id
 -- Get all Movies and their data for the display Movies page and for dropdowns
-SELECT Movies.id, title, genre, director, runTimMins, metacritic FROM Movies
-JOIN People ON Movies.id=seenMovies.movies_id;
+SELECT * FROM Movies
+SELECT Movies.id, name FROM Movies
+JOIN seenMovies ON Movies.id = seenMovies.movies_id
+JOIN People on seenMovies.people_id = People.id
 -- Get all Shows and their data for the display Shows page and for dropdowns
-SELECT Shows.id, title, genre, network, episodes, seasons, metacritic FROM Shows
-JOIN People ON Shows.id=seenShows.shows_id;
+SELECT * FROM Shows
+SELECT Shows.id, name FROM Shows
+JOIN seenShows ON Shows.id = seenShows.shows_id
+JOIN People on seenShows.people_id = People.id
 -- Get all Books and their data for the display Movies page and for dropdowns
-SELECT Books.id, title, genre, author, pages, metacritic FROM Books
-JOIN People ON Books.id=readBooks.books_id;
+SELECT * FROM Books
+SELECT Books.id, name FROM Books
+JOIN readBooks ON Books.id = readBooks.books_id
+JOIN People on readBooks.people_id = People.id
 -- Get all Video Games and their data for the display Movies page and for dropdowns
-SELECT VideoGames.id, title, genre, studio, playTimeHrs, metacritic FROM VideoGames
-JOIN People ON VideoGames.id=playedGames.video_games_id;
+SELECT * FROM VideoGames
+SELECT VideoGames.id, name FROM VideoGames
+JOIN playedGames ON VideoGames.id = playedGames.video_games_id
+JOIN People on playedGames.people_id = People.id
 
 
 -- Add a new Person
-INSERT INTO People (name, age, favMovie, favShow, favBook, favGame) VALUES (:nameInput, :ageInput, :movie_idInput, :show_idInput, :book_idInput, :game_idInput);
+INSERT INTO People (name, age, favMovie, favShow, favBook, favGame)
+VALUES ("{request.form['name']}", {request.form['age']}, {request.form['favMovie']}, {request.form['favShow']}, {request.form['favBook']}, {request.form['favGame']});
 -- Add a new Movie
-INSERT INTO Movies (title, genre, director, runTimeMins, metacritic) VALUES (:titleInput, :genreInput, :directorInput, :runTimeMinsInput, :metacriticInput);
+INSERT INTO Movies (title, genre, director, runTimeMins, metacritic)
+VALUES ("{request.form['title']}", "{request.form['genre']}", "{request.form['director']}", {request.form['runTimeMins']}, {request.form['metacritic']});
 -- Add a new Show
-INSERT INTO Shows (title, genre, network, episodes, seasons, metacritic) VALUES (:titleInput, :genreInput, :networkInput, :episodesInput, :seasonsInput, :metacriticInput);
+INSERT INTO Shows (title, genre, network, episodes, seasons, metacritic)
+VALUES ("{request.form['title']}", "{request.form['genre']}", "{request.form['network']}", {request.form['episodes']}, {request.form['seasons']}, {request.form['metacritic']});
 -- Add a new Book
-INSERT INTO Books (title, genre, author, pages, metacritic) VALUES (:titleInput, :genreInput, :authorInput, :pagesInput, :metacriticInput);
+INSERT INTO Books (title, genre, author, pages, metacritic)
+VALUES ("{request.form['title']}", "{request.form['genre']}", "{request.form['author']}", {request.form['pages']}, {request.form['metacritic']});
 -- Add a new Video Game
-INSERT INTO VideoGames (title, genre, studio, playTimeHrs, metacritic) VALUES (:titleInput, :genreInput, :studioInput, :playTimeHrsInput, :metacriticInput);
-
+INSERT INTO VideoGames (title, genre, studio, playTimeHrs, metacritic)
+VALUES ("{request.form['title']}", "{request.form['genre']}", "{request.form['studio']}", {request.form['playTimeHrs']}, {request.form['metacritic']});
+ 
 
 -- Associate a Person with a seen Movie (M:M)
-INSERT INTO seenMovies (people_id, movies_id) VALUES (:people_idInput, :movies_idInput);
+INSERT INTO seenMovies (people_id, movies_id)
+VALUES ({request.form['people_id']}, {request.form['movies_id']});
 -- Associate a Person with a seen Show (M:M)
-INSERT INTO seenShows (people_id, shows_id) VALUES (:people_idInput, :shows_idInput);
+INSERT INTO seenShows (people_id, shows_id)
+VALUES ({request.form['people_id']}, {request.form['shows_id']});
 -- Associate a Person with a read Book (M:M)
-INSERT INTO readBooks (people_id, books_id) VALUES (:people_idInput, :books_idInput);
+INSERT INTO readBooks (people_id, books_id)
+VALUES ({request.form['people_id']}, {request.form['books_id']});
 -- Associate a Person with a played Video Game (M:M)
-INSERT INTO playedGames (people_id, video_games_id) VALUES (:people_idInput, :video_games_idInput);
+INSERT INTO playedGames (people_id, video_games_id)
+VALUES ({request.form['people_id']}, {request.form['video_games_id']});
 
 
 -- update a Person's data based on submission of the Update Person form
