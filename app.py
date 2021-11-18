aa23d063450cef7dc15347969e4bf6b922474f8b
Xinnji <xinnji@gmail.com>
2021-11-17 19:53:41 -0800

    The website works now

--- app.py
+++ app.py
@@ -18,11 +18,11 @@ cursor = con.cursor(MySQLdb.cursors.DictCursor)
 # cursor.execute('query')
 # con.commit();
 
-people = ["id", "name", "age", "favMovie", "favShow", "favBook", "favGame", "seenMovies", "seenShows", "readBooks", "playedGames"]
-movies = ["id", "title", "genre", "director", "runTimeMins", "metacritic"]
-shows = ["id", "title", "genre", "network", "episodes", "seasons", "metacritic"]
-books = ["id", "title", "genre", "author", "pages", "metacritic"]
-videogames = ["id", "title", "genre", "studio", "playTimHrs", "metacritic"]
+People = ["id", "name", "age", "favMovie", "favShow", "favBook", "favGame", "seenMovies", "seenShows", "readBooks", "playedGames"]
+Movies = ["id", "title", "genre", "director", "runTimeMins", "metacritic", "Seen-it"]
+Shows = ["id", "title", "genre", "network", "episodes", "seasons", "metacritic", "Seen-it"]
+Books = ["id", "title", "genre", "author", "pages", "metacritic", "Read-it"]
+VideoGames = ["id", "title", "genre", "studio", "playTimHrs", "metacritic", "Played-it"]
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -54,79 +54,103 @@ def index():
 
     if request.method == "POST":
         cursor.execute(f'''INSERT INTO People (name, age, favMovie, favShow, favBook, favGame)
-            VALUES ("{request.form['name']}", {request.form['age']}, {request.form['favMovie']}, {request.form['favShow']}, {request.form['favBook']}, {request.form['favGame']});''')
+                        VALUES ("{request.form['name']}", {request.form['age']}, {request.form['favMovie']}, {request.form['favShow']}, {request.form['favBook']}, {request.form['favGame']});''')
         con.commit();
         return redirect('/')
 
-    return render_template('index.html', entity=people, result=result)
+    return render_template('index.html', entity=People, result=result)
 
 
 @app.route('/movies', methods=["GET", "POST"])
 def movies():
     result = []
-    queries = []
+    queries = [
+        '''SELECT * FROM Movies''',
+        '''SELECT Movies.id, name FROM Movies
+            JOIN seenMovies ON Movies.id = seenMovies.movies_id
+            JOIN People on seenMovies.people_id = People.id'''
+    ]
     for query in queries:
         cursor.execute(query)
         result.append(cursor.fetchall())
     # print(result)
 
     if request.method == "POST":
-        cursor.execute()
+        cursor.execute(f'''INSERT INTO Shows (title, genre, director, runTimeMins, metacritic)
+                        VALUES ("{request.form['title']}", "{request.form['genre']}", "{request.form['director']}", {request.form['runTimeMins']}, {request.form['metacritic']});''')
         con.commit();
         return redirect('/movies')
 
-    return render_template('movies.html', entity=movies, result=result)
+    return render_template('movies.html', entity=Movies, result=result)
 
 
 @app.route('/shows', methods=["GET", "POST"])
 def shows():
     result = []
-    queries = []
+    queries = [
+        '''SELECT * FROM Shows''',
+        '''SELECT Shows.id, name FROM Shows
+            JOIN seenShows ON Shows.id = seenShows.shows_id
+            JOIN People on seenShows.people_id = People.id'''
+    ]
     for query in queries:
         cursor.execute(query)
         result.append(cursor.fetchall())
     # print(result)
 
     if request.method == "POST":
-        cursor.execute()
+        cursor.execute(f'''INSERT INTO Shows (title, genre, network, episodes, seasons, metacritic)
+                        VALUES ("{request.form['title']}", "{request.form['genre']}", "{request.form['network']}", {request.form['episodes']}, {request.form['seasons']}, {request.form['metacritic']});''')
         con.commit();
         return redirect('/shows')
 
-    return render_template('shows.html', entity=shows, result=result)
+    return render_template('shows.html', entity=Shows, result=result)
 
 
 @app.route('/books', methods=["GET", "POST"])
 def books():
     result = []
-    queries = []
+    queries = [
+        '''SELECT * FROM Books''',
+        '''SELECT Books.id, name FROM Books
+            JOIN readBooks ON Books.id = readBooks.books_id
+            JOIN People on readBooks.people_id = People.id'''
+    ]
     for query in queries:
         cursor.execute(query)
         result.append(cursor.fetchall())
     # print(result)
 
     if request.method == "POST":
-        cursor.execute()
+        cursor.execute(f'''INSERT INTO Shows (title, genre, author, pages, metacritic)
+                        VALUES ("{request.form['title']}", "{request.form['genre']}", "{request.form['author']}", {request.form['pages']}, {request.form['metacritic']});''')
         con.commit();
         return redirect('/books')
 
-    return render_template('books.html', entity=books, result=result)
+    return render_template('books.html', entity=Books, result=result)
 
 
 @app.route('/videogames', methods=["GET", "POST"])
 def videogames():
     result = []
-    queries = []
+    queries = [
+        '''SELECT * FROM VideoGames''',
+        '''SELECT VideoGames.id, name FROM VideoGames
+            JOIN playedGames ON VideoGames.id = playedGames.video_games_id
+            JOIN People on playedGames.people_id = People.id'''
+    ]
     for query in queries:
         cursor.execute(query)
         result.append(cursor.fetchall())
     # print(result)
 
     if request.method == "POST":
-        cursor.execute()
+        cursor.execute(f'''INSERT INTO Shows (title, genre, studio, playTimeHrs, metacritic)
+                        VALUES ("{request.form['title']}", "{request.form['genre']}", "{request.form['studio']}", {request.form['playTimeHrs']}, {request.form['metacritic']});''')
         con.commit();
         return redirect('/videogames')
 
-    return render_template('videogames.html', entity=videogames, result=result)
+    return render_template('videogames.html', entity=VideoGames, result=result)
 
 
 if __name__ == '__main__':
